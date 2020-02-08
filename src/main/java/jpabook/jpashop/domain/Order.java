8f2d92a04aa03570e96a2c3b2f4ff3aee847adc1
SujinHuh <juin483@naver.com>
2020-02-08 17:44:51 +0900

    * 도메인 분석 설계(요구사항 분석, 도메인 모델과 테이블 설계, 엔티티 클래스 개발)     - 엔티티 클래스 개발 - 3
               엔티티에는 가급적 setter 사용자제
               모든 연관관계는 지연로딩으로 설정
               컬렉션은 필드에서 초기화
               연관관계 편의 메서드

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -9,22 +9,26 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import static javax.persistence.FetchType.*;
+
 @Entity(name = "orders")
-@Getter @Setter
+@Getter
+@Setter
 public class Order {
 
-    @Id @GeneratedValue
+    @Id
+    @GeneratedValue
     @Column(name = "order_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne
+    @OneToOne(fetch = LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
@@ -33,4 +37,24 @@ public class Order {
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status; // 주문상태<Order, Cancel>
+
+//   === 연관관계 편의 메서드 <원자적으로 묶는 메서드> ===
+//    위치 양쪽에 있으면 '핵심적으로 컨트롤하는 쪽'
+
+    public void setMember(Member member) {
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
+
 }
+
