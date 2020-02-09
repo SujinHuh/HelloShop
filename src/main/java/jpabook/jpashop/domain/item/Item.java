f103f97c73ba05a8b7f71ad4ee0731c17cfbbee0
SujinHuh <juin483@naver.com>
2020-02-09 17:55:04 +0900

    * 상품 도메인 개발 - 상품 엔티티 개발(비지니스 로직 추)
      구현 기능
        상품 수정
    
      - 값을 변경해야 할 일이 있다면, 핵심 비지니스메서드를 가지고 변경을 해야한다. setter로 값을 변경하는것은 좋지 않은 방

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,8 +12,7 @@ import java.util.List;
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE) //상속관맵핑 부무테이블에 맵핑 해줘야 함,싱글테이블 전략
 @DiscriminatorColumn(name = "dtype")
-@Getter @Setter
-
+@Getter
 public abstract class Item {
 
     @Id
@@ -30,4 +30,27 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+
+    //===비지니스 로직 ==//
+    /**
+     * stock 증가
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * stock 감소
+     */
+    public void removeStock(int quantity) {
+
+        int restStock = this.stockQuantity - quantity;
+
+        if(restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
+
 }
