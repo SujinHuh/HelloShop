6f04c3c83072268589772787b956323078a6635a
SujinHuh <juin483@naver.com>
2020-02-13 23:21:36 +0900

    * 웹 계층 개발 - 상품 등록 , 상품 목록

---
+++ src/main/java/jpabook/jpashop/controller/Movie.java
@@ -0,0 +1,18 @@
+package jpabook.jpashop.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class Movie {
+
+    private Long id;
+
+    private String name;
+    private int price;
+    private int stockQuantity;
+
+    private String director;
+    private String actor;
+
+}
